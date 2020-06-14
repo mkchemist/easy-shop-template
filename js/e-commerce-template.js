c4afc7a1b55ea6723c26c176ebfcf7d20884cf77
Mohamed Kamal <m.k_chemist@yahoo.com>
2020-06-14 23:53:26 +0200

    update account page

--- js/e-commerce-template.js
+++ js/e-commerce-template.js
@@ -1,21 +1,23 @@
-function FeaturedBox(t) {
-    this.root = t, this.links = $(t + " .featured-nav-link"), this.items = $(t + " .featured-item"), 
+var _links = $(".toggleEdit");
+
+function FeaturedBox(e) {
+    this.root = e, this.links = $(e + " .featured-nav-link"), this.items = $(e + " .featured-item"), 
     this.init = function() {
         this.markLinkAsActive(), this.showActiveItem();
         var s = this;
-        this.links.each(function(t, e) {
-            var i = $(e).data("index");
-            $(e).click(function(t) {
-                t.preventDefault(), s.markLinkAsActive(i), s.showActiveItem(i);
+        this.links.each(function(e, t) {
+            var i = $(t).data("index");
+            $(t).click(function(e) {
+                e.preventDefault(), s.markLinkAsActive(i), s.showActiveItem(i);
             });
         });
     }, this.markLinkAsActive = function(i) {
-        i = i || 0, this.links.each(function(t, e) {
-            $(e).parent().removeClass("active"), t === i && $(e).parent().addClass("active");
+        i = i || 0, this.links.each(function(e, t) {
+            $(t).parent().removeClass("active"), e === i && $(t).parent().addClass("active");
         });
     }, this.showActiveItem = function(i) {
-        i = i || 0, this.items.each(function(t, e) {
-            $(e).hide(), t === i && ($(e).show(), $(e).addClass("fade-effect-enter"));
+        i = i || 0, this.items.each(function(e, t) {
+            $(t).hide(), e === i && ($(t).show(), $(t).addClass("fade-effect-enter"));
         });
     };
 }
@@ -31,5 +33,12 @@ $(document).ready(function() {
         autoplay: !0,
         autoplayTimeout: 5e3
     });
-}), new FeaturedBox(".featured-box").init();
+}), new FeaturedBox(".featured-box").init(), _links && _links.each(function(e, i) {
+    i.addEventListener("click", function(e) {
+        e.preventDefault();
+        var t = i.previousElementSibling;
+        !0 === t.disabled ? ($(t).removeClass("border-bottom").addClass("border border-danger").css("border-radius", "5px"), 
+        t.disabled = !1) : (t.disabled = !0, $(t).addClass("border-bottom").removeClass("border border-danger").css("border-radius", "0px"));
+    });
+});
 //# sourceMappingURL=e-commerce-template.js.map
