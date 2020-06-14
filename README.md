a5f7494ec6c9be2aed62e1a816f7f0fed530f2bd
Mohamed Kamal <m.k_chemist@yahoo.com>
2020-06-14 02:24:03 +0200

    init

---
+++ README.md
@@ -0,0 +1,10 @@
+# EasyShop E-commerce template
+
+> Easyshop is simple html and css e-commerce website template, i hope you enjoy it.
+
+## screenshots
+-----------------------------
+### @pc media
+![large screen](./screenshots/full-size-lg.png)
+### @mobile media
+![mobile media](./screenshots/sm.png)
---
+++ js/e-commerce-template.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    $(".hero-section").owlCarousel({
+        loop: !0,
+        items: 1,
+        center: !0,
+        dots: !0,
+        dotsClass: "carousel-dots",
+        lazyLoad: !0,
+        autoplay: !0,
+        autoplayTimeout: 5e3
+    });
+});
+//# sourceMappingURL=e-commerce-template.js.map
